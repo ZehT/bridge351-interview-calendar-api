771e72bc4d96116a84ff9d1fd938a2b9631fe554
trein <jhtrein@gmail.com>
2022-08-27 16:58:14 +0100

    project configuration - read me - spring boot - swagger - h2 - liquibase - git ignore

---
+++ src/main/java/com/bridge351/interviewcalendarapi/InterviewCalendarApiApplication.java
@@ -0,0 +1,20 @@
+package com.bridge351.interviewcalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@EnableCaching
+@ComponentScan(lazyInit = true)
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+public class InterviewCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCalendarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridge351/interviewcalendarapi/InterviewCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge351.interviewcalendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
