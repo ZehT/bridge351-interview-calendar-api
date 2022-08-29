d319b8897257251843d346c1e9cb17336e467acf
trein <jhtrein@gmail.com>
2022-08-29 23:34:16 +0100

    [TASK#5] messages configuration - usage of the message

--- src/main/java/com/bridge351/interviewcalendarapi/person/api/candidate/CandidateController.java
+++ src/main/java/com/bridge351/interviewcalendarapi/person/api/candidate/CandidateController.java
@@ -5,15 +5,20 @@ import com.bridge351.interviewcalendarapi.person.PersonService;
 import com.bridge351.interviewcalendarapi.person.domain.PersonDTO;
 import com.bridge351.interviewcalendarapi.person.domain.PersonEntity;
 import com.bridge351.interviewcalendarapi.person.enums.PersonTypeEnum;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class CandidateController implements CandidateAPI {
 
-    final PersonService personService;
+    private final PersonService personService;
+    private final MessageSource messageSource;
 
-    public CandidateController(final PersonService personService) {
+    public CandidateController(final PersonService personService, final MessageSource messageSource) {
         this.personService = personService;
+        this.messageSource = messageSource;
     }
 
     @Override
@@ -21,7 +26,7 @@ public class CandidateController implements CandidateAPI {
         final PersonEntity personEntity = this.personService.addPerson(PersonEntity.ofDTO(personDTO, PersonTypeEnum.CANDIDATE.getId()));
         return BasicResponse.withDataAndMessage(
                 PersonDTO.ofEntity(personEntity),
-                "OK"
+                messageSource.getMessage("person.candidate.created", null, Locale.getDefault())
         );
     }
 
